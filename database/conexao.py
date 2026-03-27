5434104a7f0cb8376ad049280686307accbdda02
oliviawyy <olivia.c@aluno.senai.br>
2026-03-27 15:42:50 -0300

    Update conexao.py

--- database/conexao.py
+++ database/conexao.py
@@ -3,11 +3,22 @@ import mysql.connector
 
 def conectar():
 
-    conexao = mysql.connector.connect(
+    tipo_conexao = "NUVEM"
+    if tipo_conexao == "LOCAL":
+        conexao = mysql.connector.connect(
         host="127.0.0.1",
         port="3306",
         user="root",
         password="root",
+        database="MusicTube"
+        
+    )
+    else:
+        conexao = mysql.connector.connect(
+        host="servidor-olivia-servidor-olivia.a.aivencloud.com",
+        port="22681",
+        user="avnadmin",
+        password="AVNS_J6yhXvAVBZ0f6eyeelN",
         database="MusicTube"
         
     )
